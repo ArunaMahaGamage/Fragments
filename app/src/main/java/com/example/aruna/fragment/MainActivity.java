151d1551517925fe128c26ec073577cc15f005ea
Aruna <admgbit@gmail.com>
2019-02-26 13:06:47 +0530

    first commit

---
+++ app/src/main/java/com/example/aruna/fragment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.aruna.fragment;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startFragment();
+    }
+
+    private void startFragment() {
+
+        ArticleFragment newFragment = new ArticleFragment();
+        Bundle args = new Bundle();
+
+        newFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+}
